5885f4de5b4502862cf35afca4f2c6d35257ba68
praveena <mnk9522@gmail.com>
2019-09-27 13:05:27 +0530

    qwerty

---
+++ src/EvenList.java
@@ -0,0 +1,14 @@
+
+public class EvenList {
+
+	public static void main(String[] args) {
+		for (int e = 1; e <= 50; e++) {
+			if (e % 2 != 0) {
+				System.out.println("even number is" + e);
+			} else {
+				System.out.println("odd number is " + e);
+			}
+		}
+	}
+
+}
